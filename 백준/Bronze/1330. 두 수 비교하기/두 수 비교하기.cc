9a29118c55222dda76a4f09509a50ad08d672424
Park Minyoung <62882579+pmy02@users.noreply.github.com>
2022-07-17 16:52:30 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a,b;
+
+int main()
+{
+    cin>>a>>b;
+    if(a>b) printf(">");
+    if(a<b) printf("<");
+    if(a==b) printf("==");
+    return 0;
+}
