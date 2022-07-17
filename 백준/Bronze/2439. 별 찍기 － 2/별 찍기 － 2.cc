1f40467f5c1b023698a8b928cea74937d0539e44
Park Minyoung <62882579+pmy02@users.noreply.github.com>
2022-07-17 17:11:13 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,i,j;
+
+int main()
+{
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=0;j<n-i;j++)
+            printf(" ");
+        for(j=0;j<i;j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
