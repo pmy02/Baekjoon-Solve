db4b8788e4f22ac73dd027478118de1fcf7ba1e8
Park Minyoung <62882579+pmy02@users.noreply.github.com>
2022-07-17 17:15:19 +0900

    [Unrated] Title: 별 찍기 - 14, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/2556. 별 찍기 － 14/README.md
@@ -0,0 +1,12 @@
+# [Unrated] 별 찍기 - 14 - 2556 
+
+[문제 링크](https://www.acmicpc.net/problem/2556) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Unrated/2556. 별 찍기 － 14/별 찍기 － 14.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,i,j;
+
+int main()
+{
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
