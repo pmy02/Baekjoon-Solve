55ea860f1bcf2e88d2373af13078a9397a25e615
Park Minyoung <62882579+pmy02@users.noreply.github.com>
2022-07-17 17:21:50 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+string s;
+
+
+int main()
+{
+    cin>>s;
+
+    for(int i=0;i<s.size();i++)
+        if(s[i]>=65&&s[i]<=90) s[i]+=32;
+        else s[i]-=32;
+
+    cout<<s;
+    return 0;
+}
