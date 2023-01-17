c94cfc25926d8f2f7a4c141598a4c89f94365759
Park Minyoung <62882579+pmy02@users.noreply.github.com>
2023-01-17 20:34:36 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+
+int main()
+{
+    while(1){
+        scanf("%d %d",&n,&m);
+        if(n==0&&m==0) break;
+        printf("%d\n",n+m);
+    }
+
+    return 0;
+}
