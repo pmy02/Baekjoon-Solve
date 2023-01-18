8a5fb8e0ab0a7ad990cd4e6a5f760ac8eea5c761
Park Minyoung <62882579+pmy02@users.noreply.github.com>
2023-01-18 15:22:51 +0900

    [Bronze IV] Title: 0123456789, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6916. 0123456789/0123456789.cc
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main()
+{
+    cin>>n;
+    
+    if(n==0){
+        printf(" * * *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf(" * * *\n");
+    }
+    else if(n==1){
+        printf("\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("\n");
+    }
+    else if(n==2){
+        printf(" * * *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf(" * * *\n");
+        printf("*\n");
+        printf("*\n");
+        printf("*\n");
+        printf(" * * *\n");
+    }
+    else if(n==3){
+        printf(" * * *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf(" * * *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf(" * * *\n");
+    }
+    else if(n==4){
+        printf("\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf(" * * *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("\n");
+    }
+    else if(n==5){
+        printf(" * * *\n");
+        printf("*\n");
+        printf("*\n");
+        printf("*\n");
+        printf(" * * *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf(" * * *\n");
+    }
+    else if(n==6){
+        printf(" * * *\n");
+        printf("*\n");
+        printf("*\n");
+        printf("*\n");
+        printf(" * * *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf(" * * *\n");
+    }
+    else if(n==7){
+        printf(" * * *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("\n");
+    }
+    else if(n==8){
+        printf(" * * *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf(" * * *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf(" * * *\n");
+    }
+    else if(n==9){
+        printf(" * * *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf(" * * *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf("      *\n");
+        printf(" * * *\n");
+    }
+    
+    return 0;
+}
