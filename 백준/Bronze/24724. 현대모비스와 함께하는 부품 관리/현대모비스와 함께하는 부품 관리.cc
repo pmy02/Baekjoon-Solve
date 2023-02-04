e44955edf273eaf032197328f90c84572fa882bf
Park Minyoung <62882579+pmy02@users.noreply.github.com>
2023-02-05 00:33:16 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int i,n;
+
+int main()
+{
+    cin>>n;
+    for(i=1;i<=n;i++){
+        
+        printf("Material Management %d\n",i);
+        printf("Classification ---- End!\n");
+    }
+
+    return 0;
+}
