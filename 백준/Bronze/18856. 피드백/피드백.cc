7171d6985c4f24b18062a8e103b581f07e423078
Park Minyoung <62882579+pmy02@users.noreply.github.com>
2023-02-05 01:10:44 +0900

    [Bronze III] Title: 피드백, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18856. 피드백/피드백.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int i,n;
+
+int main()
+{
+    cin>>n;
+    
+    printf("%d\n",n);
+    for(i=1;i<n;i++){
+        printf("%d ",i);
+    }
+    printf("53");
+    
+    return 0;
+}
